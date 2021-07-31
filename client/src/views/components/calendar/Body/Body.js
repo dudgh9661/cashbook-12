2ab86508bc98267f5ec8c29024346b82e5aaadf6
Seogeurim <geulims@naver.com>
2021-08-01 02:34:04 +0900

    feat: :zap: 캘린더 바디 마크업
    - 날짜를 나타내는 캘린더 바디 마크업
    - 임시로 클릭할 때마다 달이 바뀌도록 설정

--- client/src/views/components/calendar/Body/Body.js
+++ client/src/views/components/calendar/Body/Body.js
@@ -1,5 +1,10 @@
 import Component from '@lib/Component';
+import { DateInfo } from '@store';
 import './Body.scss';
+
+const prevMonthHandler = () => {
+  DateInfo.setPrevMonth();
+};
 
 class CalendarBody extends Component {
   constructor() {
@@ -8,15 +13,43 @@ class CalendarBody extends Component {
     this.init();
   }
 
+  setObserver() {
+    DateInfo.observe('current', this.reRender.bind(this));
+  }
+
   render() {
-    const $calendarBody = document.createElement('div');
+    const { current } = DateInfo.state;
+
+    const $calendarBody = document.createElement('table');
     $calendarBody.className = 'calendar-body';
 
-    $calendarBody.innerHTML = `
-      <h1>Calendar Body</h1>
-    `;
+    let date = -current.firstDay + 1;
+    while (date <= current.date) {
+      const $dayRow = document.createElement('tr');
+      $dayRow.className = 'calendar-body__week';
+
+      for (let day = 0; day < 7; day += 1) {
+        $dayRow.innerHTML += `
+          <td class="calendar-body__day">
+            <div class="day-content">
+              <span></span>
+              <span class="day-content__num">
+                ${date > 0 && date <= current.date ? date : ''}
+              </span>
+            </div>
+          </td>
+        `;
+        date += 1;
+      }
+
+      $calendarBody.appendChild($dayRow);
+    }
 
     return $calendarBody;
+  }
+
+  setEvent() {
+    this.addEvent('click', '.calendar-body', prevMonthHandler);
   }
 }
 
