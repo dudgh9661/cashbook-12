614c21d8d5bb8b23363b26e0fb47ab78721a7dda
Seogeurim <geulims@naver.com>
2021-08-01 02:45:39 +0900

    feat: :zap: 캘린더 푸터 마크업
    월간 총 통계를 나타내는 캘린더 푸터 마크업

--- client/src/views/components/calendar/Footer/Footer.js
+++ client/src/views/components/calendar/Footer/Footer.js
@@ -13,7 +13,15 @@ class CalendarFooter extends Component {
     $calendarFooter.className = 'calendar-footer';
 
     $calendarFooter.innerHTML = `
-      <h1>Calendar Footer</h1>
+      <div class="calendar-footer__left">
+        <div class="calendar-footer__left--pr">
+          총 수입 <span>1,822,480</span>
+        </div>
+        <div>총 지출 <span>834,640</span></div>
+      </div>
+      <div class="calendar-footer__right">
+        총계 <span>987,840</span>
+      </div>
     `;
 
     return $calendarFooter;
