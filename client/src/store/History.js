dc3f4f01c221a4836a09c6807af831b69a160a3e
Seogeurim <geulims@naver.com>
2021-08-02 01:14:48 +0900

    refactor: :sparkles: History model 함수 네이밍 변경
    - history 모델에서 history state를 set하는 역할의 함수 네이밍 변경
    - 현재 선택된 달의 history 정보를 가져오는 함수 분리

--- client/src/store/History.js
+++ client/src/store/History.js
@@ -2,17 +2,20 @@ import Observable from '@lib/Observable';
 import DateInfo from './DateInfo';
 import historyData from '../_dummies/history.json';
 
-class History extends Observable {
-  getCurrentMonthHistory() {
-    const { year, month } = DateInfo.state.current;
-    const dataName = `data-${year}-${month}`;
+const getCurrentMonthHistory = () => {
+  const { year, month } = DateInfo.state.current;
+  const dataName = `data-${year}-${month}`;
 
-    this.state.history = historyData[dataName];
+  return historyData[dataName];
+};
+
+class History extends Observable {
+  setCurrentMonthHistory() {
+    this.state.history = getCurrentMonthHistory();
   }
 
-  getCurrentMonthTotal() {
-    const { year, month } = DateInfo.state.current;
-    const data = historyData[`data-${year}-${month}`];
+  setCurrentMonthTotal() {
+    const data = getCurrentMonthHistory();
 
     this.state.total = {
       income: data ? data.reduce((acc, cur) => acc + cur.income, 0) : 0,
