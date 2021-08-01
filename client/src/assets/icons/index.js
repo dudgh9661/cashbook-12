d4b24b295e9fc3c259dfc0853f25184afab6d3d9
dongqui <dongqui9@gmail.com>
2021-08-02 02:10:29 +0900

    feat: :zap: Modal 추가

--- client/src/assets/icons/index.js
+++ client/src/assets/icons/index.js
@@ -4,3 +4,4 @@ export { default as check } from './check';
 export { default as fileText } from './file-text';
 export { default as chevronDown } from './chevron-down';
 export { default as minus } from './minus';
+export { default as cancel } from './cancel';
---
+++ client/src/views/components/form/Modal/Modal.js
@@ -0,0 +1,42 @@
+import Component from '@lib/Component';
+import './Modal.scss';
+
+const closeModal = () => {
+  const $modal = document.querySelector('.modal');
+  $modal.classList.remove('open');
+};
+
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.init();
+  }
+
+  render() {
+    const $modal = document.createElement('div');
+    $modal.classList.add('modal');
+
+    $modal.innerHTML = `
+      <div class="modal-input">
+        <header>
+          추가하실 결제수단을 적어주세요.
+        </header>
+        <main>
+          <input placeholder="입력하세요">
+        </main>
+        <footer>
+          <button type="button" id="modal-cancel" class="cancel">취소</button>
+          <button type="button" id="modal-confirm" class="confirm">등록</button>
+        </footer>
+      </div>
+    `;
+    return $modal;
+  }
+
+  setEvent() {
+    this.addEvent('click', '#modal-cancel', closeModal);
+    this.addEvent('click', '#modal-confirm', this.props.callback);
+  }
+}
+export default Modal;
