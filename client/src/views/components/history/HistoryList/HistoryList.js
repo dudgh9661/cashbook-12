1052acf25fd9c54ee5f64d38fb41544453e7e7d2
dongqui <dongqui9@gmail.com>
2021-08-02 15:46:23 +0900

    feat: :zap: 히스토리 더미 데이터 적용
    - ERD 보고 최대한 맞춰보긴했는데 수정이 필요할 거 같습니다!
    - 임의로 사용

--- client/src/views/components/history/HistoryList/HistoryList.js
+++ client/src/views/components/history/HistoryList/HistoryList.js
@@ -1,5 +1,6 @@
 import Component from '@lib/Component';
 import { HistoryItem } from '@components';
+import historyList from '../../../../_dummies/historyList.json';
 import './HistoryList.scss';
 
 class HistoryList extends Component {
@@ -10,44 +11,15 @@ class HistoryList extends Component {
   }
 
   render() {
-    const dummyData = {
-      timestamp: '2021-07-15',
-      historyItemList: [
-        {
-          category: { text: '문화/여가', color: '#D092E2' },
-          description: '스트리밍 서비스 정기 결제',
-          paymentMethod: '현대카드',
-          amount: -10990,
-        },
-        {
-          category: { text: '교통', color: '#94D3CC' },
-          description: '후불 교통비 결제',
-          paymentMethod: '현대카드',
-          amount: -45340,
-        },
-        {
-          category: { text: '미분류', color: '#817DCE' },
-          description: '온라인 세미나 신청',
-          paymentMethod: '현대카드',
-          amount: -10000,
-        },
-      ],
-    };
-    const $historyList = document.createElement('ul');
-    $historyList.append(
+    const historyItemList = Object.entries(historyList).map(([date, list]) =>
       new HistoryItem({
-        timestamp: dummyData.timestamp,
-        historyItemList: dummyData.historyItemList,
-      }).getElement(),
-      new HistoryItem({
-        timestamp: dummyData.timestamp,
-        historyItemList: dummyData.historyItemList,
-      }).getElement(),
-      new HistoryItem({
-        timestamp: dummyData.timestamp,
-        historyItemList: dummyData.historyItemList,
+        timestamp: date,
+        historyItemList: list,
       }).getElement(),
     );
+
+    const $historyList = document.createElement('ul');
+    $historyList.append(...historyItemList);
 
     return $historyList;
   }
