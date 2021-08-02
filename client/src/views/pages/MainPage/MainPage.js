fee810a6ef6cc2adcdb9b4aed115f48658cedbea
dongqui <dongqui9@gmail.com>
2021-08-02 19:10:54 +0900

    wip: : gear: login 테스트 UI

--- client/src/views/pages/MainPage/MainPage.js
+++ client/src/views/pages/MainPage/MainPage.js
@@ -1,6 +1,5 @@
 import Component from '@lib/Component';
 import { Layout, Form, History } from '@components';
-import $ from '@utils/dom';
 import './MainPage.scss';
 import categoryData from '../../../_dummies/category.json';
 import paymentMethod from '../../../_dummies/paymentMethod.json';
@@ -13,19 +12,30 @@ class MainPage extends Component {
   }
 
   render() {
-    return $(
-      'div',
-      { class: 'main-page' },
-      new Layout({
-        children: [
-          new Form({
-            categories: categoryData.categories,
-            paymentMethods: paymentMethod.methods,
-          }),
-          new History(),
-        ],
-      }),
+    const $mainPage = document.createElement('div');
+    $mainPage.className = 'main-page';
+
+    const $loginTest = document.createElement('a');
+    $loginTest.textContent = '로그인 테스트';
+    $loginTest.href =
+      'https://github.com/login/oauth/authorize?client_id=04986225983a20a0d8c4';
+
+    const $mainPageFrag = document.createDocumentFragment();
+    $mainPageFrag.append(
+      $loginTest,
+      new Form({
+        categories: categoryData.categories,
+        paymentMethods: paymentMethod.methods,
+      }).getElement(),
+      new History({}).getElement(),
     );
+    $mainPage.appendChild(
+      new Layout({
+        children: [$mainPageFrag],
+      }).getElement(),
+    );
+
+    return $mainPage;
   }
 }
 
