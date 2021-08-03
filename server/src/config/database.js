f6dd450042f870abf1c09cf2ef15b631dfcfcffe
Seogeurim <geulims@naver.com>
2021-08-03 17:32:03 +0900

    chore: :wrench: babel을 이용한 node.js 환경 구성
    기존에 esm 라이브러리를 사용해 node.js에서 es6 문법을 사용했었는데, babel을 사용해 환경설정 변경

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,32 +2,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const env = process.env.NODE_ENV || 'development';
+
 export default {
-  development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: 3306,
-    dialect: 'mysql',
-    dialectOptions: {
-      connectTimeout: 1000,
-      typeCast: true,
-    },
-  },
-  test: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: 3306,
-    dialect: 'mysql',
-    dialectOptions: {
-      connectTimeout: 1000,
-      typeCast: true,
-    },
-  },
-  production: {
+  [env]: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
