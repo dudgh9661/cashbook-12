d35dc26adafe8fc2faa3f80d2f8b7801abac0a5c
dongqui <dongqui9@gmail.com>
2021-08-04 20:59:27 +0900

    :refactor :sparkles: api 컨벤션 통일
    - 소문자로 변경

--- client/src/store/User.js
+++ client/src/store/User.js
@@ -1,10 +1,10 @@
 import Observable from '@lib/Observable';
-import Api from '@utils/api';
+import api from '@utils/api';
 
 class User extends Observable {
   async setUser() {
     try {
-      const user = await Api.fetchUser();
+      const user = await api.fetchUser();
       this.state.user = user;
     } catch (err) {
       console.log(err);
