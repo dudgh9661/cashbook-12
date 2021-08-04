8bc472e5ae1638f5470762bad860f3791e19a799
Seogeurim <geulims@naver.com>
2021-08-05 01:16:56 +0900

    remove: :wastebasket: 불필요한 코드 삭제
    - 중복되는 util 함수 삭제
    - 더미 데이터 삭제

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -4,14 +4,6 @@ export const moneyWithComma = money => {
   return money.toLocaleString('ko-KR');
 };
 
-export const uuid = () => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r && 0x3) || 0x8;
-    return v.toString(16);
-  });
-};
-
 export const getDateFromString = date => {
   const dateObj = new Date(date);
   const days = DAYS_OF_WEEK;
