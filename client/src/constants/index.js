a0fd79002a9a42b42b531916fb7eaa501327a351
Seogeurim <geulims@naver.com>
2021-08-06 06:47:53 +0900

    chore: :wrench: 깃허브 클라이언트 아이디 환경변수화

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,11 +1,11 @@
+import env from '@config/env';
 import { fileText, calendar, chart } from '@assets/icons';
 
 export const DAYS_OF_WEEK = ['일', '월', '화', '수', '목', '금', '토'];
 
 export const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-export const GITHUB_REDIRECT_URL =
-  'https://github.com/login/oauth/authorize?client_id=04986225983a20a0d8c4';
+export const GITHUB_REDIRECT_URL = `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}`;
 
 export const NAV_LINK = {
   MAIN: { value: 'main', link: '/', icon: fileText },
